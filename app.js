21ab90d79893680e8fb381842e4d272ca88a012b
miguelyang123 <1547104+miguelyang123@users.noreply.github.com>
2022-11-11 13:58:45 +0800

    app.js update

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ var express = require("express");
 var port = process.env.PORT || 3000;
 var app = express(),
   path = require("path"),
-  publicDir = path.join(__dirname, "public");
+  publicDir = path.join(__dirname, "build");
 
 app.use(express.static(publicDir));
-app.get("/", (req, res) => {
-  res.send("Test Web.");
-});
-// app.use(express.static(path.join(__dirname, "build")));
 // app.get("/", (req, res) => {
-//   res.send(path.join(__dirname, "build", "index.html"));
+//   res.send("Test Web.");
 // });
+app.get("/", (req, res) => {
+  res.send(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port);
 module.exports = app;
